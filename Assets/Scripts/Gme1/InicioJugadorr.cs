91ef6e2231e39ea2d3d883f17f0fededbf572a86
Isaac <isaacrodacu11@gmail.com>
2023-11-20 00:56:24 -0600

    selec personajes

--- Assets/Scripts/Gme1/InicioJugadorr.cs
+++ Assets/Scripts/Gme1/InicioJugadorr.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class InicioJugadorr : MonoBehaviour
 {
+    public Transform[] spawnPoints; // Un arreglo para almacenar los puntos de spawn
+
     private void Start()
     {
-        int indexJugador = PlayerPrefs.GetInt("JugadorIndex");
-        Instantiate(GameManager.Instance.personajes[indexJugador].personajeJugable, transform.position, Quaternion.identity);
+        int indexJugador1 = PlayerPrefs.GetInt("Jugador1Index");
+        int indexJugador2 = PlayerPrefs.GetInt("Jugador2Index");
+
+        if (indexJugador1 >= 0 && indexJugador1 < GameManager.Instance.personajes.Count &&
+            indexJugador2 >= 0 && indexJugador2 < GameManager.Instance.personajes.Count &&
+            spawnPoints.Length >= 2)
+        {
+            GameObject jugador1 = Instantiate(GameManager.Instance.personajes[indexJugador1].personajeJugable, spawnPoints[0].position, Quaternion.identity);
+            GameObject jugador2 = Instantiate(GameManager.Instance.personajes[indexJugador2].personajeJugable, spawnPoints[1].position, Quaternion.identity);
+
+            // Cambiar la orientación del personaje del jugador 2 hacia la izquierda
+            if (jugador2 != null)
+            {
+                // Obtener el componente de escala (scale) del Transform
+                Vector3 scale = jugador2.transform.localScale;
+                // Voltear el personaje hacia la izquierda
+                scale.x = -Mathf.Abs(scale.x);
+                // Aplicar la nueva escala al Transform
+                jugador2.transform.localScale = scale;
+            }
+        }
     }
+
 }
--- Assets/Scripts/Gme1/MenuSeleccion.cs
+++ Assets/Scripts/Gme1/MenuSeleccion.cs
@@ -5,65 +5,93 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-
 public class MenuSeleccion : MonoBehaviour
 {
-    private int index;
+    public GameObject canvasPersonajes;
+    public GameObject canvasDificultad;
 
-    [SerializeField] private Image imagen;
-    [SerializeField] private TextMeshProUGUI nombre;
+    public RawImage[] introVideo;
+
+    [SerializeField] private Image[] imagenes; // Arreglo de imágenes para los personajes de los jugadores.
+    [SerializeField] private TextMeshProUGUI[] nombres; // Arreglo de TextMeshProUGUI para los nombres de los personajes.
+
     private GameManager gameManager;
+    private int[] index; // Arreglo de índices de selección para los dos jugadores.
+
 
     private void Start()
     {
         gameManager = GameManager.Instance;
 
-        index = PlayerPrefs.GetInt("JugadorIndex");
+        canvasPersonajes.SetActive(true);
+        canvasDificultad.SetActive(false);
 
-        if(index > gameManager.personajes.Count - 1)
+        index = new int[2]; // Inicializar arreglo de índices para los dos jugadores.
+        index[0] = PlayerPrefs.GetInt("Jugador1Index"); // Obtener índice del jugador 1 desde PlayerPrefs.
+        index[1] = PlayerPrefs.GetInt("Jugador2Index"); // Obtener índice del jugador 2 desde PlayerPrefs.
+
+        // Verificar si los índices están dentro del rango de personajes disponibles.
+        for (int i = 0; i < 2; i++)
         {
-            index = 0;
+            if (index[i] > gameManager.personajes.Count - 1 || index[i] < 0)
+            {
+                index[i] = 0; // Si el índice está fuera de rango, establecer el primer personaje por defecto.
+            }
+            CambiarPantalla(i);
         }
-
-        CambiarPantalla();
     }
 
-    private void CambiarPantalla()
+    private void CambiarPantalla(int jugador)
     {
-        PlayerPrefs.SetInt("JugadorIndex", index);
-        imagen.sprite = gameManager.personajes[index].imagen;
-        nombre.text = gameManager.personajes[index].nombre;
+        PlayerPrefs.SetInt("Jugador" + (jugador + 1) + "Index", index[jugador]); // Guardar el índice del jugador.
+
+        imagenes[jugador].sprite = gameManager.personajes[index[jugador]].imagen;
+        nombres[jugador].text = gameManager.personajes[index[jugador]].nombre;
     }
 
-    public void SigPersonaje()
+    public void SigPersonaje(int jugador)
     {
-        if(index == gameManager.personajes.Count - 1)
-        {
-            index = 0;
-        }
-        else
-        {
-            index += 1;
-        }
-        CambiarPantalla();
+        index[jugador] = (index[jugador] + 1) % gameManager.personajes.Count; // Cambiar al siguiente personaje circularmente.
+        CambiarPantalla(jugador);
+        PlayerPrefs.SetInt("Jugador" + (jugador + 1) + "Index", index[jugador]); // Guardar el índice del jugador.
     }
 
-    public void AnteriorPersonaje()
+    public void AnteriorPersonaje(int jugador)
     {
-        if (index == 0)
-        {
-            index = gameManager.personajes.Count - 1;
-        }
-        else
-        {
-            index -= 1;
-        }
-        CambiarPantalla();
+        index[jugador] = (index[jugador] - 1 + gameManager.personajes.Count) % gameManager.personajes.Count; // Cambiar al anterior personaje circularmente.
+        CambiarPantalla(jugador);
+        PlayerPrefs.SetInt("Jugador" + (jugador + 1) + "Index", index[jugador]); // Guardar el índice del jugador.
+    }
+
+    public void ChangeNextCanvas()
+    {
+        canvasPersonajes.SetActive(false);
+        canvasDificultad.SetActive(true);
+    }
+
+    public void ChangeLastCanvas()
+    {
+        canvasPersonajes.SetActive(true);
+        canvasDificultad.SetActive(false);
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(PlayIntroAndStartGame());
     }
 
+    IEnumerator PlayIntroAndStartGame()
+    {
+
+        // Activar el RawImage y reproducir el video o GIF correspondiente a la combinación de personajes.
+        introVideo[0].gameObject.SetActive(true);
+        // Aquí carga y reproduce el video o GIF según la combinación de personajes elegida por los jugadores.
+
+        // Esperar durante el tiempo necesario para mostrar la introducción.
+        yield return new WaitForSeconds(3f);
+
+        // Después de la presentación, desactivar el RawImage y cargar la siguiente escena.
+        introVideo[0].gameObject.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
