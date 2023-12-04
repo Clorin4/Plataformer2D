ce5640e93008cb962296c43505e3a39bc743c9fc
Isaac <isaacrodacu11@gmail.com>
2023-12-04 00:56:40 -0600

    YA CASIII

--- Assets/Scripts/Game1/GameManager.cs
+++ Assets/Scripts/Game1/GameManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public List<Personajes> personajes;
 
-    //private string selectedDifficulty;
+
 
     private void Awake()
     {
@@ -22,5 +23,20 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    
+
+    public void Salir()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+        // Esto solo se ejecutará en el Editor de Unity para simular la salida del juego
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
 }
