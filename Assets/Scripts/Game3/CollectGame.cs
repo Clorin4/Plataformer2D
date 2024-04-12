3dd6cf908ccf1946579e90580660ad019064b3f9
Isaac <127635276+Clorin4@users.noreply.github.com>
2024-04-12 14:14:20 -0600

    Isaac1204

---
+++ Assets/Scripts/Game3/CollectGame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        Debug.Log("OLA CARA DE BOLA");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
