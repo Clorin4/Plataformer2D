6d69a2a669779c193b6980bda04f20c8f18c976f
Canela8 <estefivv@gmail.com>
2024-05-18 20:37:39 -0600

    18/05 cambios

--- Assets/Scripts/Game1/VNewSP.cs
+++ Assets/Scripts/Game1/VNewSP.cs
@@ -4,12 +4,63 @@ using UnityEngine;
 
 public class PersonjesNS : MonoBehaviour
 {
-    public Animator ObjectAnim;
+    //Zorrito activity
+    private Animator ObjectAnim;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        ObjectAnim = GetComponent<Animator>();
     }
+
+    //Zorrito nada
+    public void StartNothing()
+    {
+        StartCoroutine(Nothing());
+    }
+    //zorrito pierde
+    public void StartLose()
+    {
+        StartCoroutine(Lose());
+    }
+    //zorrito habla
+    public void StartTalk()
+    {
+        StartCoroutine(Talk());
+    }
+    //zorrito gana
+    public void StartWin()
+    {
+        StartCoroutine(Win());
+    }
+    //-----Coroutines-----
+    IEnumerator Nothing()
+    {
+        ObjectAnim.SetBool("Parpadea", true);
+        yield return new WaitForSeconds(1.5f);
+    }
+
+    IEnumerator Lose()
+    {
+        ObjectAnim.SetBool("Pierde", true);
+        yield return new WaitForSeconds(1.0f);
+        ObjectAnim.SetBool("Pierde", false);
+    }
+
+    IEnumerator Talk()
+    {
+        ObjectAnim.SetBool("Hablando", true);
+        yield return new WaitForSeconds(1.0f);
+        ObjectAnim.SetBool("Hablando", false);
+    }
+
+    IEnumerator Win()
+    {
+        ObjectAnim.SetBool("Check", true);
+        yield return new WaitForSeconds(1.0f);
+        ObjectAnim.SetBool("Check", false);
+    }
+    
 
     // Update is called once per frame
     void Update()
